625d040ee6a96dcb1bde072a252375bd1bc3fdd9
Ed Ball <ed.ball@logos.com>
2016-12-19 17:56:22 -0800

    Support parsing Swagger.

--- src/Facility.GeneratorApi.Services/Properties/AssemblyInfo.cs
+++ src/Facility.GeneratorApi.Services/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyVersion("0.2.0")]
 [assembly: AssemblyProduct("Facility.GeneratorApi.Services")]
 [assembly: ComVisible(false)]
